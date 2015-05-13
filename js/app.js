2e8dd48850d9879955fd25201c377ea7bf225092
Earl Carlson <theearlcarlson@gmail.com>
2015-05-12 23:50:50 -0400

    images and things

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+$(function() {
+
+var image;
+console.log('ne')
+$('li').mouseover(function(){
+	image = $(this).data('artist')
+	$('.bg').css({'background-image':'url(images/' + image + '.jpg)'})
+});
+
+});
