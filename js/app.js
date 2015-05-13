25f0e0e2a6fc989ebb3ae7010bbe56dcdf5ac302
Earl Carlson <theearlcarlson@gmail.com>
2015-05-13 09:17:41 -0400

    images and things

--- js/app.js
+++ js/app.js
@@ -3,9 +3,16 @@ $(function() {
 
 var image;
 console.log('ne')
-$('li').mouseover(function(){
+$('.artists li').mouseover(function(){
 	image = $(this).data('artist')
 	$('.bg').css({'background-image':'url(images/' + image + '.jpg)'})
 });
+$('#intro').mouseover(function(){
+	image = $(this).data('artist')
+	$('.bg').css({'background-image':'none'})
+});
 
+$( window ).scroll(function() {
+	$('#down').fadeOut();
+});
 });
