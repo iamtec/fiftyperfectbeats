bd20eb7953f05e20d7bb227d641f4f77840d1015
Earl Carlson <theearlcarlson@gmail.com>
2015-05-15 09:25:58 -0400

    done

--- js/app.js
+++ js/app.js
@@ -1,18 +1,32 @@
-$(document).foundation();
+	
 $(function() {
 
-var image;
-console.log('ne')
+var image, eachImg, appender;
 $('.artists li').mouseover(function(){
 	image = $(this).data('artist')
-	$('.bg').css({'background-image':'url(images/' + image + '.jpg)'})
+	$('.bg').removeClass('active');
+	$('.bg2 li').removeClass('active');
+	$('.bg2 li[data-artist="' + image + '"]').addClass('active');
 });
 $('#intro').mouseover(function(){
-	image = $(this).data('artist')
-	$('.bg').css({'background-image':'none'})
+	image = $(this).data('artist');
+	$('.bg2 li').removeClass('active');
+	$('.bg').addClass('active');
 });
 
 $( window ).scroll(function() {
 	$('#down').fadeOut();
 });
+$('.artists li').each(function(){
+	eachImg = $(this).data('artist');
+	appender = '<li data-artist="' + eachImg + '" style="background-image:url(images/' + eachImg + '.jpg);">s</li>'
+	$('.bg, .bg2').append(appender);
 });
+
+
+
+});
+$(window).load(function(){
+	$("#intro").addClass('loaded');
+	$('.loader-inner').fadeOut();
+})
